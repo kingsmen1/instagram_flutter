3b2ead913a885ad6a1c6df900e61dd9fd82f3372
osmanghori <90970163+kingsmen1@users.noreply.github.com>
2022-06-01 18:39:18 +0530

    day1_firebase added_IOS_Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/resonsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter/resonsive/responsive_layout_screen.dart';
+import 'package:instagram_flutter/resonsive/web_screen_layout.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Myapp());
 }
 
@@ -14,6 +21,13 @@ class Myapp extends StatefulWidget {
 class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: ResponsiveLayout(
+            webScreenLayout: WebScreenLayout(),
+            mobileScreenLayout: MobileScreenLayout()));
   }
 }
